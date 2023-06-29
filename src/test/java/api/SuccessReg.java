c678afa5dfe60e342287ecdd996edd7cfe9da45e
Borisov-Ilya <borisov.ilya22@gmail.com>
2023-06-29 21:06:22 +0300

    Добавил API тесты с POJO классами

---
+++ src/test/java/api/SuccessReg.java
@@ -0,0 +1,30 @@
+package api;
+
+public class SuccessReg {
+    private Integer id;
+    private String token;
+
+    public SuccessReg(Integer id, String token) {
+        this.id = id;
+        this.token = token;
+    }
+
+    public SuccessReg() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
